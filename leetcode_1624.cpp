6d1945dd542ef38d1b6f279c1f64cf13f107435c
Deekshith G L <117536596+DeekshithGL@users.noreply.github.com>
2023-12-31 18:01:50 +0530

    Create leetcode_1624.cpp

---
+++ leetcode_1624.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maxLengthBetweenEqualCharacters(string s) {
+        int result = -1;
+        map<char, vector<int>> mpp;
+
+        for(auto i = 0; i < s.size(); i++)
+            mpp[s[i]].push_back(i);
+        
+        for(auto it : mpp)
+        {
+            if(it.second.size() > 1)
+            {        
+                result = max(result, it.second[it.second.size() - 1] - it.second[0]);
+            }
+        }
+
+        if(result == -1)
+            return result;
+        return result - 1;
+    }
+};
