9df5c85cbf2baacc4b9949b1cbdcae79b11105b6
Deekshith G L <117536596+DeekshithGL@users.noreply.github.com>
2024-08-19 15:43:34 +0530

    Time: 8 ms (11.13%), Space: 16.9 MB (5.02%) - LeetHub

---
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -0,0 +1,27 @@
+class Solution {
+private:
+    void solve(vector<int> nums, set<vector<int>>& res, vector<int> op, int idx)
+    {
+        if(idx == nums.size())
+        {
+            res.insert(op);
+            return ;
+        }
+
+        solve(nums, res, op, idx + 1);
+        
+        op.push_back(nums[idx]);
+        solve(nums, res, op, idx + 1);
+    }
+
+public:
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        set<vector<int>> result;
+        vector<int> op;
+        sort(nums.begin(), nums.end());
+        solve(nums, result, op, 0);
+        vector<vector<int>> res2(result.begin(), result.end());
+        
+        return res2;
+    }
+};
