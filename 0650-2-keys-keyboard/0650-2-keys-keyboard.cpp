1bfe147a87f374dfa683b511934fc3e80b9297db
Deekshith G L <117536596+DeekshithGL@users.noreply.github.com>
2024-08-19 16:31:50 +0530

    Time: 3 ms (69.17%), Space: 7.5 MB (71.95%) - LeetHub

---
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int minSteps(int n) {
+        int steps = 0;
+
+        for(int i = 2; i * i <= n; i++)
+        {
+            while(n % i == 0)
+            {
+                steps += i;
+                n /= i;
+            }
+        }
+
+        if(n > 1)
+            steps += n;
+
+        return steps;
+    }
+};
